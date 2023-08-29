b9ade80acc0b6f16d76852403db7ac329ce8eef8
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-29 10:26:13 -0600

    got blinking lights

---
+++ ard_test.ino
@@ -0,0 +1,26 @@
+int yPin = 11;
+int rPin = 12;
+int gPin = 13;
+long blink = 200;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(yPin, OUTPUT);
+    pinMode(rPin, OUTPUT);
+    pinMode(gPin, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(yPin, HIGH);
+    delay(blink);
+    digitalWrite(rPin, HIGH);
+    delay(blink);
+    digitalWrite(gPin, HIGH);
+    delay(blink);
+    digitalWrite(yPin, LOW);
+    delay(blink);
+    digitalWrite(rPin, LOW);
+    delay(blink);
+    digitalWrite(gPin, LOW);
+    delay(blink);
+}
