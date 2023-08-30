48f054daf22dec317bfd8e3ebb696b6f6c07e95c
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-30 10:03:02 -0600

    added button input to test

--- ard_test.ino
+++ ard_test.ino
@@ -1,3 +1,4 @@
+int inPin = 8;
 int yPin = 11;
 int rPin = 12;
 int gPin = 13;
@@ -5,22 +6,25 @@ long blink = 200;
 
 void setup() {
     Serial.begin(9600);
+    pinMode(inPin, INPUT);
     pinMode(yPin, OUTPUT);
     pinMode(rPin, OUTPUT);
     pinMode(gPin, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(yPin, HIGH);
-    delay(blink);
-    digitalWrite(rPin, HIGH);
-    delay(blink);
-    digitalWrite(gPin, HIGH);
-    delay(blink);
-    digitalWrite(yPin, LOW);
-    delay(blink);
-    digitalWrite(rPin, LOW);
-    delay(blink);
-    digitalWrite(gPin, LOW);
-    delay(blink);
+    while (digitalRead(inPin) == HIGH) {
+        digitalWrite(yPin, HIGH);
+        delay(blink);
+        digitalWrite(rPin, HIGH);
+        delay(blink);
+        digitalWrite(gPin, HIGH);
+        delay(blink);
+        digitalWrite(yPin, LOW);
+        delay(blink);
+        digitalWrite(rPin, LOW);
+        delay(blink);
+        digitalWrite(gPin, LOW);
+        delay(blink);
+    }
 }
